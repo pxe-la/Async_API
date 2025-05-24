8cf26742bc70add361b8ca959c78b48a9736c761
apetrovichev <apetrovichev@tradingview.com>
2025-05-24 21:13:18 +0400

    002: Films service and api

--- services/api/src/models/film.py
+++ services/api/src/models/film.py
@@ -1,8 +1,26 @@
-# Используем pydantic для упрощения работы при перегонке данных из json в объекты
+from typing import Set
+from uuid import UUID
+
 from pydantic import BaseModel
+
+from .genre import Genre
+from .person import Person
 
 
 class Film(BaseModel):
-    id: str
+    id: UUID
     title: str
-    description: str
+    description: str | None
+    imdb_rating: float | None
+
+    genres: Set[Genre]
+    genre_names: Set[str]
+
+    actors: Set[Person]
+    actors_names: Set[str]
+
+    directors: Set[Person]
+    directors_names: Set[str]
+
+    writers: Set[Person]
+    writers_names: Set[str]
