9b45c44fc175ab337fddb1df6fd8cadab70c9ec7
zakhy <lirchzakhar@gmail.com>
2025-05-31 18:27:20 +0700

    parametrize & fixture

--- services/api/tests/functional/src/test_search.py
+++ services/api/tests/functional/src/test_search.py
@@ -1,15 +1,54 @@
 import uuid
-
+import asyncio
 import aiohttp
 import pytest
+import pytest_asyncio
 from elasticsearch import AsyncElasticsearch
 from elasticsearch.helpers import async_bulk
 
 from tests.functional.settings import test_settings
 
-@pytest.mark.asyncio
-async def test_search():
-    # 1. Генерируем данные для ES
+
+@pytest_asyncio.fixture(scope='session')
+def _function_event_loop():
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture(name="es_client", scope="session")
+async def es_client():
+    es_client = AsyncElasticsearch(hosts=test_settings.es_host,
+                                   verify_certs=False)
+    yield es_client
+    await es_client.close()
+
+
+@pytest_asyncio.fixture(name="client_http_session", scope="session")
+async def client_http_session():
+    session = aiohttp.ClientSession()
+    yield session
+    await session.close()
+
+
+@pytest_asyncio.fixture(name="make_get_request")
+async def make_get_request(client_http_session):
+    async def inner(url, query_data):
+        full_url = test_settings.service_url + "/" + url
+
+        async with client_http_session.get(full_url,
+                                           params=query_data) as response:
+            response_dict = {
+                "body": await response.json(),
+                "status": response.status
+            }
+        return response_dict
+
+    return inner
+
+
+@pytest_asyncio.fixture(name="es_data")
+async def es_data():
     es_data = [{
         "id": str(uuid.uuid4()),
         "imdb_rating": 8.5,
@@ -44,34 +83,45 @@ async def test_search():
             "_id": row["id"],
             "_source": row
         })
+    return bulk_query
 
-        # 2. Загружаем данные в ES
-    es_client = AsyncElasticsearch(hosts=test_settings.es_host,
-                                   verify_certs=False)
-    if await es_client.indices.exists(index=test_settings.es_index):
-        await es_client.indices.delete(index=test_settings.es_index)
-    await es_client.indices.create(index=test_settings.es_index,
-                                   **test_settings.es_index_mapping)
 
-    updated, errors = await async_bulk(client=es_client, actions=bulk_query)
-    await es_client.indices.refresh(index=test_settings.es_index)
-    await es_client.close()
+@pytest_asyncio.fixture(name="es_write_data")
+def es_write_data(es_client):
+    async def inner(data: list[dict]):
+        if await es_client.indices.exists(index=test_settings.es_index):
+            await es_client.indices.delete(index=test_settings.es_index)
+        await es_client.indices.create(index=test_settings.es_index,
+                                       **test_settings.es_index_mapping)
+        updated, errors = await async_bulk(client=es_client, actions=data)
+        await es_client.indices.refresh(index=test_settings.es_index)
 
-    if errors:
-        raise Exception("Ошибка записи данных в Elasticsearch")
+        if errors:
+            raise Exception('Ошибка записи данных в Elasticsearch')
 
-    # 3. Запрашиваем данные из ES по API
+    return inner
 
-    session = aiohttp.ClientSession()
-    url = test_settings.service_url + "/api/v1/films/search"
-    query_data = {"query": "The Star"}
-    async with session.get(url, params=query_data) as response:
-        body = await response.json()
-        headers = response.headers
-        status = response.status
-    await session.close()
 
-    # 4. Проверяем ответ
+@pytest.mark.parametrize(
+    "query_data, expected_answer",
+    [
+        (
+                {"query": "The Star"},
+                {"status": 200, "length": 50}
+        ),
+        (
+                {"query": "Mashed Potato"},
+                {"status": 200, "length": 0}
+        )
+    ]
+)
+@pytest.mark.asyncio
+async def test_search(make_get_request, es_write_data, es_data: list[dict],
+                      query_data,
+                      expected_answer):
+    await es_write_data(es_data)
 
-    assert status == 200
-    assert len(body) == 50
+    response = await make_get_request("api/v1/films/search", query_data)
+
+    assert response["status"] == expected_answer["status"]
+    assert len(response["body"]) == expected_answer["length"]
