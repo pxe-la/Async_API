91ef08e4bb0262e62c393a885fb238a12f8ca590
zakhy <lirchzakhar@gmail.com>
2025-06-12 16:25:16 +0700

    01 create storage protocol

--- services/api/src/services/storage.py
+++ services/api/src/services/storage.py
@@ -1,6 +1,8 @@
-from typing import Protocol
+from typing import Protocol, Optional, Any
 
 from elasticsearch import AsyncElasticsearch
+
+from models.film import Film
 
 
 class StorageServiceProtocol(Protocol):
@@ -11,16 +13,17 @@ class StorageServiceProtocol(Protocol):
             **kwargs: str
     ) -> list[dict]: ...
 
-    async def get_list(self, resource: str, page_size: int,
-                       page_number: int) -> list[dict]: ...
+    async def get_list(
+            self, resource: str, page_size: int, page_number: int
+    ) -> list[dict]: ...
 
 
-class ElasticsearchService(StorageServiceProtocol):
+class BaseElasticsearchService(StorageServiceProtocol):
     def __init__(self, elastic: AsyncElasticsearch):
         self.elastic = elastic
 
     async def get(self, resource: str, uuid: str) -> dict:
-        return self.elastic.get(index=resource, id=uuid)
+        return await self.elastic.get(index=resource, id=uuid)
 
     async def search(
             self, resource: str, page_size: int, page_number: int,
@@ -34,8 +37,9 @@ class ElasticsearchService(StorageServiceProtocol):
 
         return await self.elastic.search(index=resource, body=search_query)
 
-    async def get_list(self, resource: str, page_size: int,
-                       page_number: int) -> list[dict]:
+    async def get_list(
+            self, resource: str, page_size: int, page_number: int
+    ) -> list[dict]:
         search_query = {
             "query": {"match_all": {}},
             "size": page_size,
@@ -43,3 +47,34 @@ class ElasticsearchService(StorageServiceProtocol):
         }
 
         return await self.elastic.search(index=resource, body=search_query)
+
+
+class FilmElasticSearchService(BaseElasticsearchService):
+    INDEX = "movies"
+
+    async def search(
+            self,
+            query: dict[str, Any],
+            page_size: Optional[int] = None,
+            page_number: Optional[int] = None,
+            sort: Optional[str] = None,
+            **kwargs: str,
+    ) -> list[dict]:
+
+        body: dict[str, Any] = {
+            "query": query,
+        }
+
+        if page_size:
+            body["size"] = page_size
+
+        if page_number and page_size:
+            body["from"] = (page_number - 1) * page_size
+
+        if sort is not None:
+            sort_field = sort.lstrip("-")
+            order = "desc" if sort.startswith("-") else "asc"
+            body["sort"] = [{sort_field: {"order": order}}]
+
+        response = await self.elastic.search(index=self.INDEX, body=body)
+        return [Film(**hit["_source"]) for hit in response["hits"]["hits"]]
