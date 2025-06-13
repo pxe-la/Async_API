866efbcbf876a327221c23fefbf0677a05386172
zakhy <lirchzakhar@gmail.com>
2025-06-13 18:27:05 +0700

    23 add backoff to ping files

--- services/api/tests/functional/utils/wait_for_es.py
+++ services/api/tests/functional/utils/wait_for_es.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 import os
-import time
 
+from backoff import backoff
 from elasticsearch import Elasticsearch
+
+
+@backoff()
+def ping_es(es: Elasticsearch):
+    es.info()
+
 
 if __name__ == "__main__":
     es_url = os.getenv("ES_URL", "http://localhost:9200")
     es_client = Elasticsearch(es_url)
-    res = es_client.info()
-    while True:
-        if es_client.ping():
-            break
-        time.sleep(1)
+    ping_es(es_client)
--- services/api/tests/functional/utils/wait_for_redis.py
+++ services/api/tests/functional/utils/wait_for_redis.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 import os
-import time
 
-from redis import Redis, exceptions
+from backoff import backoff
+from redis import Redis
+
+
+@backoff()
+def ping_redis(redis: Redis):
+    redis.ping()
+
 
 if __name__ == "__main__":
     redis_host = os.getenv("REDIS_HOST", "127.0.0.1")
     r = Redis(redis_host, socket_connect_timeout=1)
 
-    while True:
-        try:
-            r.ping()
-            break
-        except exceptions.ConnectionError:
-            print("Ping redis attempt: ", redis_host)
-            time.sleep(1)
+    ping_redis(r)
